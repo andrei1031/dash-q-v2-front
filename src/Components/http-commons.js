4450cc872ce8d975aebdc491bd3aaef1cd0833e6
Reuben Andrei Saldivar <andreisantossaldivar@gmail.com>
2026-02-10 22:29:01 +0800

    Guest login

--- src/Components/http-commons.js
+++ src/Components/http-commons.js
@@ -1,2 +1,2 @@
 //export const API_URL = process.env.REACT_APP_API_URL
-export const API_URL = "https://dash-q-v2-back.onrender.com"
+export const API_URL = "https://dash-q-v2-back.onrender.com/api"
