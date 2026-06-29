312be984239f0ad20958d7737da9e42bc79f5b8c
Reuben Andrei Saldivar <andreisantossaldivar@gmail.com>
2026-06-29 23:06:54 +0800

    Fix admin route reference error

--- src/Components/admin/StaffManagement.js
+++ src/Components/admin/StaffManagement.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { API_URL } from "../../http-commons";
-import { supabase } from "../../supabase";
+import { API_URL } from "../http-commons";
+import { supabase } from "../supabase";
 
 export const StaffManagement = ({ session }) => {
     const [staffList, setStaffList] = useState([]);
